61c9b1dfedc922277a446cacee05607217ca64ed
andy <chaochaoqi@gmail.com>
2018-05-23 15:16:39 -0700

    Connected AWS EC2 instance

--- server/index.js
+++ server/index.js
@@ -8,7 +8,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-AWS.config.update({region:'us-east-1'});
+AWS.config.update({region:'us-west-1'});
+
+ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+let params = {
+    InstanceIds: ['i-09c7d8998b882f858']
+};
+
+ec2.describeInstances(params, function(err, data) {
+    if (err) {
+      console.log("Error", err.stack);
+    } else {
+      console.log("Success", JSON.stringify(data));
+    }
+});
 
 let server = app.listen(5000, function () {
     let port = server.address().port;
