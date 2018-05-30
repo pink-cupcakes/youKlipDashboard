25c188f61fb44875da1cd4996da09f8ea45957a4
Anna <annaoh1994@gmail.com>
2018-05-29 23:42:42 -0700

    Add sort bar for home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,12 +8,31 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <iframe width="560" height="315"
-          src="https://www.youtube.com/embed/V62pGsjKNKQ"
-          frameborder="0"
-          allow="autoplay; encrypted-media" allowfullscreen>
-        </iframe>
-        
+
+        <div className="home-sort-bar">
+          <div className="sort-chosen">Hot</div>
+          <div className="">Fresh</div>
+          <div className="">Troll</div>
+          <div className="">Controversial</div>
+          <div className="sort-bar-leftover"></div>
+        </div>
+
+        <div className="main-video-holder">
+          <div className="main-video-description">
+            <div className="main-video-userimg"></div>
+            <div>
+              <h2>Username</h2>
+              <h3>playing League of Legends</h3>
+            </div>
+          </div>
+          <iframe
+            className="main-video"
+            src="https://www.youtube.com/embed/V62pGsjKNKQ?&autoplay=1&mute=1"
+            frameborder="0"
+            allow="autoplay; encrypted-media" allowfullscreen>
+          </iframe>
+        </div>
+
 
       </div>
     );
