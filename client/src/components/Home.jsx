1274f0a7b63957cea822e131e45f62bf28c44d10
Anna <annaoh1994@gmail.com>
2018-06-04 14:36:56 -0700

    test styling for home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -49,22 +49,80 @@ class Home extends Component {
           </iframe>
         </div>
 
-        <div className="side-video-holder">
-          <div className="side-video-description-holder">
-            <div className="side-video-title">Trolling in Overwatch</div>
-            <div className="side-video-description">
-              <div className="side-video-userimg"></div>
-              <div>
-                <h2>Username</h2>
-                <h3>playing Overwatch</h3>
+        <div className="all-side-videos">
+
+          <div className="side-video-holder">
+            <div className="side-video-description-holder">
+              <div className="side-video-title">Trolling in Overwatch</div>
+              <div className="side-video-description">
+                <div className="side-video-userimg"></div>
+                <div>
+                  <h2>Username</h2>
+                  <h3>playing Overwatch</h3>
+                </div>
               </div>
             </div>
+            <iframe
+              className="side-video"
+              src="https://www.youtube.com/embed/j1u_uvCwv5g"
+              allow="autoplay; encrypted-media; fullscreen">
+            </iframe>
           </div>
-          <iframe
-            className="side-video"
-            src="https://www.youtube.com/embed/j1u_uvCwv5g"
-            allow="autoplay; encrypted-media; fullscreen">
-          </iframe>
+
+          <div className="side-video-holder">
+            <div className="side-video-description-holder">
+              <div className="side-video-title">Trolling in Overwatch</div>
+              <div className="side-video-description">
+                <div className="side-video-userimg"></div>
+                <div>
+                  <h2>Username</h2>
+                  <h3>playing Overwatch</h3>
+                </div>
+              </div>
+            </div>
+            <iframe
+              className="side-video"
+              src="https://www.youtube.com/embed/j1u_uvCwv5g"
+              allow="autoplay; encrypted-media; fullscreen">
+            </iframe>
+          </div>
+
+          <div className="side-video-holder">
+            <div className="side-video-description-holder">
+              <div className="side-video-title">Trolling in Overwatch</div>
+              <div className="side-video-description">
+                <div className="side-video-userimg"></div>
+                <div>
+                  <h2>Username</h2>
+                  <h3>playing Overwatch</h3>
+                </div>
+              </div>
+            </div>
+            <iframe
+              className="side-video"
+              src="https://www.youtube.com/embed/j1u_uvCwv5g"
+              allow="autoplay; encrypted-media; fullscreen">
+            </iframe>
+          </div>
+
+          <div className="side-video-holder">
+              <div className="side-video-description-holder">
+                <div className="side-video-title">Trolling in Overwatch</div>
+                <div className="side-video-description">
+                  <div className="side-video-userimg"></div>
+                  <div>
+                    <h2>Username</h2>
+                    <h3>playing Overwatch</h3>
+                  </div>
+                </div>
+              </div>
+              <iframe
+                className="side-video"
+                src="https://www.youtube.com/embed/j1u_uvCwv5g"
+                allow="autoplay; encrypted-media; fullscreen">
+              </iframe>
+          </div>
+
         </div>
 
 
