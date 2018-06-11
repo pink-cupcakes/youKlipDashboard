07f679543f027e42c866cc8255ddb3c8640b6d67
Anna <annaoh1994@gmail.com>
2018-06-10 19:38:49 -0700

    Make NewsfeedVideo component

--- client/src/components/NewsfeedVideo.jsx
+++ client/src/components/NewsfeedVideo.jsx
@@ -2,30 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../assets/stylesheets/newsfeedvideo.css';
+import storiesImg1 from '../assets/images/stories-sample1.jpeg';
 
 
 class NewsfeedVideo extends Component {
   render() {
     return (
       <div className="newsfeed-video-all">
-        <div className="video-grid-item">
-          <div className="video-grid-notification"> <i className="fas fa-heart"></i> Doublelift, Sneaky and 95 others  </div>
+
+        <div className="nf-item">
+          <img  className="nf-item-notification-userimg" src={storiesImg1} />
+          <div className="nf-item-notification"> <i className="fas fa-heart"></i> Doublelift, Sneaky and 95 others  </div>
+          <h2 className="nf-item-date"> June 7, 2018 at 11:30pm</h2>
           <iframe
-            className="main-video"
+            className="nf-item-video"
             src="https://www.youtube.com/embed/tlKjC3r9yWw"
             allow="autoplay; encrypted-media; fullscreen">
           </iframe>
-          <div className="video-grid-title">Sion + Lifeline ARAM LOL</div>
-          <div className="video-grid-notification"> 865 views </div>
-          <div className="video-grid-notification"> <i className="fas fa-thumbs-up"></i> 780 </div>
-          <div className="video-grid-notification"> <i className="fas fa-thumbs-down"></i> 85 </div>
-          <div className="video-grid-userimg" ></div>
-          <h2 className="video-grid-username">Charm</h2>
-          <h3 className="video-grid-playing">playing <span className="video-grid-game">League of Legends</span></h3>
+          <div className="nf-item-title">Sion + Lifeline ARAM LOL</div>
+          <div  className="nf-item-userimg" ></div><i className="fas fa-user"></i></div>
+          <h2 className="nf-item-username">Charm</h2>
+          <h3 className="nf-item-playing">playing <span className="nf-item-game"> League of Legends </span></h3>
+          <div className="nf-item-view"> 865 views </div>
+          <div className="nf-item-tup"> <i className="fas fa-thumbs-up"></i> 780 </div>
+          <div className="nf-item-tdown"> <i className="fas fa-thumbs-down"></i> 85 </div>
+          <div className="nf-item-userimg" ></div>
+
+
+
         </div>
 
-
-      </div>
     );
   }
 }
